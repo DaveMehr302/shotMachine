b41c6fe8b147edf86ff19cdf45e4e0cf585bc471
DaveMehr <davme@gmx.ch>
2024-05-02 22:29:25 +0200

    modified Desktop, test for mouse hovering

--- test.py
+++ test.py
@@ -69,13 +69,13 @@ frame = tk.Frame(
 )
 frame.grid(row=1, column=3, padx=5, pady=5)
 
-btn_v1 = tk.Button(master=frame, text="Ventil 1", fg="white", bg="black", activebackground='black' width=10, height=5)
+btn_v1 = tk.Button(master=frame, text="Ventil 1", fg="white", bg="black", activebackground='black', width=10, height=5)
 btn_v1.pack(padx=5, pady=5, side=tk.LEFT)
 btn_v1.bind('<Button-1>', btn_v1_handle_left_click)    
-btn_v2 = tk.Button(master=frame, text="Ventil 2", fg="white", bg="black", activebackground='black' width=10, height=5)
+btn_v2 = tk.Button(master=frame, text="Ventil 2", fg="white", bg="black", activebackground='black', width=10, height=5)
 btn_v2.pack(padx=5, pady=5, side=tk.LEFT)
 btn_v2.bind('<Button-1>', btn_v2_handle_left_click)    
-btn_v3 = tk.Button(master=frame, text="Ventil 3", fg="white", bg="black", activebackground='black' width=10, height=5)
+btn_v3 = tk.Button(master=frame, text="Ventil 3", fg="white", bg="black", activebackground='black', width=10, height=5)
 btn_v3.pack(padx=5, pady=5, side=tk.LEFT)
 btn_v3.bind('<Button-1>', btn_v3_handle_left_click)    
     
